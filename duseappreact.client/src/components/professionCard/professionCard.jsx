56f954cee04298dca2337628cda6e2a79c789500
Arthur Lenkovets <Lenkovets.arthur@gmail.com>
2024-09-10 00:06:00 +0300

    ending of card slider

--- duseappreact.client/src/components/professionCard/professionCard.jsx
+++ duseappreact.client/src/components/professionCard/professionCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect , useState } from "react";
 import style from './professionCard.module.css'
 
 import arrow from './img/arrow.svg'
@@ -7,6 +7,10 @@ const professionCard = ({props}) => {
 
     const [isOpenDescription, setIsOpenDescription] = useState({ open: false })
 
+    useEffect(() => {
+        console.log(isOpenDescription);
+    }, [])
+
     return (
         <div className={style.wrapper}>
             <img src={props.img} alt="" className={style.descripionImg}/>
@@ -14,13 +18,13 @@ const professionCard = ({props}) => {
             <p className={style.description}>{props.description}</p>
             <div className={style.linkBox}>
                 <p className={style.link}>Читать дальше</p>
-                <div className={style.linkButton}  onClick={() => setIsOpenDescription({ open: !isOpenDescription.open })}>
+                <div className={style.linkButton}  onMouseEnter={() => setIsOpenDescription({ open: true })} onMouseLeave={() => setIsOpenDescription({ open: false })}>
                     <img src={arrow} alt="" className={style.linkButtonImg} />
 
-                    {isOpenDescription && 
-                    <div className={style.exDescription}>
-                        <p className={style.exDescriptionText}>{props.exDescription}</p>
-                    </div>
+                    {isOpenDescription.open && 
+                        <div className={style.exDescription}>
+                            <p className={style.exDescriptionText}>{props.exDescription}</p>
+                        </div>
                     }
                 </div>
             </div>
